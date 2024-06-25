edde4069d43ec14812f2f32058a79260e98d8d89
polidahiya830@gmail.com <polidahiya830@gmail.com>
2024-06-25 17:59:20 +0530

    .

--- app/page.js
+++ app/page.js
@@ -42,6 +42,23 @@ function page() {
       stars: 4,
     },
   ];
+  const collage = [
+    {
+      image:
+        "https://www.decorpot.com/images/500618969Living-Room-Furniture-Essentials_Main.jpg",
+      title: "Living room",
+    },
+    {
+      image:
+        "https://5.imimg.com/data5/SELLER/Default/2023/2/NZ/VW/LE/183238162/modern-l-shape-kitchen.jpg",
+      title: "Pta nhi",
+    },
+    {
+      image:
+        "https://hips.hearstapps.com/hmg-prod/images/living-room-ideas-hbx060122cover-001-1657042925.jpg?crop=1xw:1xh;center,top&resize=980:*",
+      title: "ye bda kmra lgra h",
+    },
+  ];
   return (
     <div>
       <div className="relative w-full box-content h-fit">
@@ -102,7 +119,7 @@ function page() {
         })}
       </div>
       {/* best selling products */}
-      <div className="flex items-center gap-[10px] px-[10px] md:px-[40px]">
+      <div className="flex items-center gap-[20px] px-[10px] md:px-[40px]">
         <div className="min-w-[300px]">
           <h3
             className="text-[40px] font-bold p-0"
@@ -143,119 +160,55 @@ function page() {
           );
         })}
       </div>
-      {/*  */}
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa vitae
-        consequatur iusto praesentium, animi consectetur beatae placeat
-        corrupti, molestiae quas fugiat neque, aliquid eligendi illum suscipit
-        deserunt quam ratione aperiam sapiente adipisci id maiores quo repellat
-        obcaecati! Cumque reiciendis itaque autem eaque eveniet doloribus nam
-        excepturi natus! Rem natus vero non reiciendis, harum voluptas adipisci
-        aut, praesentium fuga voluptate fugit quas doloremque illum? Ut rerum
-        quia deleniti aspernatur a distinctio deserunt excepturi asperiores
-        pariatur vero recusandae dolore amet iure, fugit dicta natus commodi
-        obcaecati adipisci alias. Architecto, enim? Consectetur, quod nulla.
-        Soluta earum iste veritatis harum fuga ducimus, voluptates ut in impedit
-        dolore quia accusamus error, expedita dolorum dolorem odit culpa
-        repellendus odio delectus quisquam quasi. Ullam illum soluta dolorum
-        veritatis atque corporis nisi omnis obcaecati sapiente! Illum aut
-        distinctio quas non officia, quaerat repellendus nobis ut quibusdam
-        suscipit mollitia, sunt animi eveniet modi amet quidem quos quia. Quidem
-        praesentium voluptatibus doloremque. Eligendi maxime laborum hic! Ea
-        enim quasi consequatur ut maiores nam commodi porro, dignissimos
-        suscipit quos culpa at? Placeat deleniti explicabo harum dolor ipsam vel
-        quo. Esse maxime tempora doloribus praesentium eligendi veritatis vero
-        aliquam, animi enim illum a sit, vitae, repudiandae impedit numquam ab
-        dignissimos soluta! Minima totam, sapiente beatae id tempore sequi cum
-        repellat nesciunt odio voluptas praesentium est libero autem
-        exercitationem perferendis repudiandae! Error, quibusdam ex temporibus
-        harum aut repellat? Assumenda suscipit, consequuntur laudantium rerum
-        repudiandae error itaque ducimus doloremque vel tempore maxime totam
-        minus temporibus saepe consequatur modi quod quaerat cumque fugit odit
-        quos velit qui sint? Fuga, alias? Dolorem porro aliquam, recusandae
-        expedita, voluptate placeat, maiores modi delectus quisquam voluptatum
-        voluptatibus doloribus quia ratione harum totam neque dignissimos
-        officia. Eum expedita nihil reprehenderit placeat illum ducimus quis
-        vitae optio facere aliquid dolorum rem similique voluptatem laborum
-        dolor natus vero incidunt, maiores tempora sunt ullam doloremque, sequi
-        deleniti sint! Nam adipisci esse sint itaque quia, aliquam modi voluptas
-        numquam natus. Quos cum nemo odio assumenda possimus, est qui odit
-        inventore, quaerat quia quidem et, molestias tempore atque corporis
-        rerum praesentium! Eligendi quisquam optio obcaecati dolores? Excepturi,
-        sunt dolor! Distinctio, at laboriosam libero tenetur ullam dolores.
-        Consectetur quam commodi dignissimos consequuntur consequatur impedit
-        tempora nostrum quis, ad aspernatur itaque laboriosam dolorum, libero
-        animi similique quasi nihil excepturi! Consectetur rem in veniam
-        cupiditate nihil excepturi maiores totam architecto? Quisquam laboriosam
-        atque cum ad! Illum iusto illo provident in saepe, harum dolor?
-        Repellendus accusantium obcaecati quidem vel. Impedit incidunt omnis
-        minus, quam explicabo et esse sed alias, distinctio praesentium numquam
-        id, recusandae iste dignissimos blanditiis debitis rerum corporis ullam!
-        Fugiat, eligendi commodi accusamus ipsum perspiciatis nam in excepturi
-        voluptatum tenetur doloribus repellat corrupti, corporis libero odit
-        similique recusandae inventore officia, atque ullam architecto quis quod
-        autem omnis magnam. Quibusdam distinctio excepturi placeat molestias cum
-        voluptatibus itaque ducimus ea nihil numquam, reiciendis fuga neque quae
-        doloribus dicta provident eveniet tempora, exercitationem eius. Ex in
-        excepturi perspiciatis sunt inventore suscipit totam. Odit reiciendis
-        eveniet veniam, labore, sint voluptas deleniti sit corporis facilis
-        velit optio non distinctio doloribus dolorum magnam eaque saepe odio, ex
-        delectus sed necessitatibus. Recusandae, dolorem excepturi possimus rem
-        nesciunt reiciendis repellat repudiandae. Quibusdam cumque nulla sit.
-        Enim, ad eos. Adipisci molestias deleniti ullam possimus consectetur
-        beatae, dolorum esse iure voluptatibus cupiditate nulla perferendis fuga
-        sapiente a nisi. Quas officia perspiciatis natus aliquam minima
-        cupiditate perferendis, quasi ipsum architecto non voluptatem accusamus
-        officiis, soluta laudantium modi placeat sequi, facere fugit accusantium
-        in dolores ducimus laborum. Cum sequi temporibus facilis repellendus
-        laboriosam libero ab nam. Alias officiis culpa iste suscipit blanditiis
-        voluptatum ducimus, quaerat ipsa unde! Beatae minus eligendi culpa eaque
-        inventore, perferendis voluptatum enim exercitationem, impedit corporis
-        similique, rem velit dignissimos commodi modi! Ut, libero tempore?
-        Dignissimos dolorum, odit itaque dolore, ullam, perspiciatis atque sed
-        suscipit quae possimus illo accusamus natus quis labore nisi modi
-        officiis explicabo vero velit reprehenderit veniam optio! Aut,
-        consequatur officia iure voluptatem, dignissimos placeat maxime
-        obcaecati rem fuga, nam impedit. Accusamus qui illum doloribus, eaque
-        dicta ducimus dolore porro delectus modi commodi ad ipsam ullam
-        voluptates excepturi quae et quia voluptatem, non aliquid. Fugit fugiat
-        doloribus minus est natus voluptatibus corporis voluptatem, nisi qui
-        molestias possimus accusamus non? Esse numquam ratione sint fugiat illo
-        natus iure minus officia, veniam id aperiam. Reiciendis est, cupiditate
-        consequuntur rerum atque temporibus natus vitae quae animi deleniti
-        nesciunt architecto sed non officiis alias saepe odit voluptate esse
-        dolorum autem porro veritatis fugiat quos quidem. Quo dolorem atque
-        libero ducimus eius ab dolores voluptates ullam! Impedit et nisi,
-        quaerat voluptatem tempore voluptas vel dolor deleniti neque vitae odit
-        aspernatur consequuntur, eos ad dolorem ullam. Est excepturi repellendus
-        laborum? Rerum ipsam amet neque alias, minus suscipit ea dolores at
-        repellendus iure ipsum molestias, dignissimos perferendis porro officiis
-        deserunt fuga consectetur doloribus minima soluta! In maiores odio
-        incidunt dolore ipsum debitis minima consectetur minus ex doloremque!
-        Praesentium soluta illo ut quisquam fugiat sequi dignissimos velit,
-        temporibus aut minima aliquam error? Ab rem iusto distinctio earum error
-        veniam a reprehenderit autem labore nostrum, exercitationem natus ipsum?
-        Architecto necessitatibus nesciunt accusantium dolores, animi sed illo?
-        Non numquam optio eligendi quos dolores? Facilis at voluptatibus sunt
-        eligendi omnis tenetur recusandae veniam modi consequuntur
-        exercitationem. Labore in placeat sit adipisci itaque, repudiandae
-        facilis beatae similique aspernatur quasi a sequi magnam blanditiis
-        consectetur enim incidunt molestiae dolorem exercitationem recusandae?
-        Eveniet id blanditiis saepe fugiat repudiandae quae ipsam eaque
-        consequatur alias quis, quo, natus reprehenderit vel enim. Saepe
-        blanditiis sint obcaecati dignissimos nam cumque, sit sequi minus
-        corporis harum similique quidem pariatur iste at suscipit. Reprehenderit
-        veritatis veniam praesentium minus, ratione aut hic neque earum at
-        labore corporis impedit cum ea sint nihil nostrum. Ad repellendus quidem
-        dolor error dolores ab commodi? Nostrum amet excepturi quae
-        exercitationem officiis doloremque facilis ipsam impedit, vitae vel
-        mollitia adipisci est. Tenetur ab harum excepturi, repellat veritatis
-        doloribus inventore, est sint aliquid quaerat, ullam consequatur
-        delectus nostrum et? Voluptates sint quas explicabo! Vitae sed
-        voluptatum doloremque ipsam nesciunt dolores ducimus accusamus ullam
-        recusandae fugit necessitatibus enim cupiditate earum quis, at deserunt
-        modi fugiat quas eveniet. Quibusdam quaerat recusandae ea.
-      </p>
+      {/* collage */}
+      <div className="px-[10px] md:px-[40px] mt-[80px]">
+        <h2 className="text-center font-bold text-[40px]">This is a heading</h2>
+        <p className="text-center font-bold text-gray-700">
+          This is a para this is a para
+        </p>
+        <div className="grid grid-cols-2 gap-[20px] mt-[30px]">
+          <div className="relative group rounded-[2vw] aspect-[4/2] overflow-hidden cursor-pointer">
+            <img
+              className="w-full h-full object-cover object-center scale-[1.2] group-hover:scale-[1] duration-700"
+              src={collage[0].image}
+              alt={collage[0].title}
+            />
+            <div className="absolute w-full h-full top-0 left-0 flex items-center justify-center bg-black bg-opacity-[50%] pointer-events-none opacity-0 group-hover:opacity-[1] duration-300">
+              <div className="text-white font-bold text-[2.5vw] translate-y-[50px] opacity-0 group-hover:opacity-[1] group-hover:translate-y-0 duration-300">
+                {collage[0].title}
+              </div>
+            </div>
+          </div>
+          <div className="relative group rounded-[2vw]  row-span-2 overflow-hidden cursor-pointer">
+            <img
+              className="w-full h-full object-cover object-center scale-[1.2] group-hover:scale-[1] duration-700"
+              src={collage[1].image}
+              alt={collage[1].title}
+            />
+            <div className="absolute w-full h-full top-0 left-0 flex items-center justify-center bg-black bg-opacity-[50%] pointer-events-none opacity-0 group-hover:opacity-[1] duration-300">
+              <div className="text-white font-bold text-[2.5vw] translate-y-[50px] opacity-0 group-hover:opacity-[1] group-hover:translate-y-0 duration-300">
+                {collage[1].title}
+              </div>
+            </div>
+          </div>
+          <div className="relative group rounded-[2vw] aspect-[4/2] overflow-hidden cursor-pointer">
+            <img
+              className="w-full h-full object-cover object-center scale-[1.2] group-hover:scale-[1] duration-700"
+              src={collage[2].image}
+              alt={collage[2].title}
+            />
+            <div className="absolute w-full h-full top-0 left-0 flex items-center justify-center bg-black bg-opacity-[50%] pointer-events-none opacity-0 group-hover:opacity-[1] duration-300">
+              <div className="text-white font-bold text-[2.5vw] translate-y-[50px] opacity-0 group-hover:opacity-[1] group-hover:translate-y-0 duration-300">
+                {collage[2].title}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* footer */}
+      <footer className="bg-[linear-gradient(110deg,#79818c,#263242)] h-[300px] mt-[50px]">
+        This i foolter
+      </footer>
     </div>
   );
 }
