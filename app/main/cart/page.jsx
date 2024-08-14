4d362b8691188c60f1f3c5700faf155896127c76
polidahiya830@gmail.com <polidahiya830@gmail.com>
2024-08-14 10:56:29 +0530

    .

--- app/main/cart/page.jsx
+++ app/main/cart/page.jsx
@@ -3,7 +3,7 @@ import Page from "./Publicpage";
 import { cookies } from "next/headers";
 
 function page() {
-  const userdata = cookies().get("userdata").value;
+  const userdata = cookies()?.get("userdata")?.value;
   return <Page userdata={userdata}/>;
 }
 
