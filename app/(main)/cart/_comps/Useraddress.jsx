85b9ed3a38e62c1861db974bbdfeb47491251daf
parvesh <polidahiya830@gmail.com>
2024-09-21 09:52:11 +0530

    /

--- app/(main)/cart/_comps/Useraddress.jsx
+++ app/(main)/cart/_comps/Useraddress.jsx
@@ -14,42 +14,50 @@ function Useraddress({ userdata }) {
         const data = await fetch(`https://api.postalpincode.in/pincode/${pin}`);
         const jsondata = await data.json();
         setpindata(jsondata[0]);
+      } else {
+        setpindata(null);
       }
     })();
   }, [pin]);
 
   return (
-    <div className=" h-[50px] flex items-center justify-between gap-[5px] bg-white border border-slate-300 p-[7px]">
-      <div className=" relative h-full flex items-center gap-[5px] px-[10px] border border-slate-300 rounded-[5px] ml-[50px]">
-        <ImLocation2 className="text-blue-600" />
+    <div className="bg-white border border-slate-300 p-[10px] lg:pl-[20px] lg:pt-[20px]">
+      <div className="relative flex items-center gap-[10px]">
+        <ImLocation2 className="text-theme" />
         <input
-          className="h-full max-w-[80px] outline-none text-[14px] text-center"
+          className="h-full outline-none "
           type="number"
           placeholder="Pin code"
+          onFocus={() => {
+            setshowpinmenu(true);
+          }}
+          onBlur={() => {
+            setshowpinmenu(false);
+          }}
           onInput={(e) => {
             setpin(e.target.value);
           }}
         />
-        {pin.length > 0 && <Fetchedatacomp pindata={pindata} />}
-      </div>
-      <div className="w-full flex items-center px-[20px] whitespace-nowrap overflow-hidden text-ellipsis text-slate-400 ">
-        Address : {userdata?.address}
+        {showpinmenu && <Fetchedatacomp pindata={pindata} pin={pin} />}
       </div>
       <Link
         href="/updateuserdetails"
-        className="h-full flex gap-[5px] items-center  bg-theme text-white  px-[20px] rounded-[5px]"
+        className="h-[35px] w-full flex items-center justify-between mt-[5px]"
       >
-        <MdOutlineEditNote className="text-[25px]" />
-        Change
+        <span> Address : {userdata?.address}</span>
+        <button className="flex items-center justify-center gap-[5px] h-[40px] md:h-full aspect-square md:aspect-auto md:px-[20px] rounded-[5px] bg-theme text-white">
+          <MdOutlineEditNote className="h-full text-[25px]" />
+          <span className="hidden md:block">Change</span>
+        </button>
       </Link>
     </div>
   );
 }
 
-function Fetchedatacomp({ pindata }) {
+function Fetchedatacomp({ pindata, pin }) {
   return (
     <div className="absolute top-[calc(100%+10px)] left-0 bg-white rounded-[5px] min-w-[300px] p-[15px] border border-slate-300 shadow-lg z-10">
-      {pindata?.Status === "Error" ? (
+      {pindata?.Status === "Error" || pin !== 6 ? (
         <div className="text-red-600 font-semibold text-center">
           Invalid Pincode
         </div>
@@ -60,15 +68,19 @@ function Fetchedatacomp({ pindata }) {
             {pindata?.PostOffice[0]?.State}
           </h2>
           <div className="flex flex-col max-h-[300px] overflow-y-scroll">
+            {pindata == null && <div className="p-2">Loading...</div>}
             {pindata?.PostOffice.map((item, i) => (
               <button
                 key={i}
-                className="flex items-center border-b border-slate-200 p-2  last:border-b-0 hover:bg-slate-100 "
+                className="flex items-center border-b border-slate-200 p-2  hover:bg-slate-100 "
               >
                 <span className="font-semibold">{item?.Name}</span>
                 <span className="ml-2 text-gray-600">- {item?.Division}</span>
               </button>
             ))}
+            <button className="p-2  font-semibold hover:bg-slate-100 text-start">
+              Not in the list
+            </button>
           </div>
         </>
       )}
