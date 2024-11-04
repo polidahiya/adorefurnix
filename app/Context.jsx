a3782813ae4d3b951182d1ddb8a4304a11e3f7a2
parvesh <polidahiya830@gmail.com>
2024-11-04 20:28:16 +0530

    go top button

--- app/Context.jsx
+++ app/Context.jsx
@@ -41,7 +41,6 @@ export function Appwrapper({ children }) {
     show: false,
     effect: false,
   });
-  const [scrollPosition, setScrollPosition] = useState(0);
 
   // admin contexts
   const [addproduct, setaddproduct] = useState(uploadproductdata);
@@ -64,21 +63,7 @@ export function Appwrapper({ children }) {
     }
   }, []);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const currentScrollY = window.scrollY;
-      if (Math.abs(currentScrollY - scrollPosition) > 100) {
-        console.log("updated",currentScrollY);
-        setScrollPosition(currentScrollY);
-      }
-    };
-
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, [scrollPosition]);
+ 
 
   return (
     <AppContext.Provider
@@ -102,7 +87,6 @@ export function Appwrapper({ children }) {
         setmessagefn,
         instantlogin,
         setinstantlogin,
-        scrollPosition,
         // admin
         addproduct,
         setaddproduct,
--- app/_components/Gotopbutton.jsx
+++ app/_components/Gotopbutton.jsx
@@ -1,11 +1,40 @@
 "use client";
-import { AppContextfn } from "../Context";
+import { useEffect, useState } from "react";
+import { FaArrowUpLong } from "react-icons/fa6";
 
 function Gotopbutton() {
-  const { scrollPosition } = AppContextfn();
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+      if (Math.abs(currentScrollY - scrollPosition) > 100) {
+        setScrollPosition(currentScrollY);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [scrollPosition]);
+
   if (scrollPosition > 1000)
     return (
-      <button className="fixed bg-pink-500 bottom-10 right-10 test h-10 aspect-square rounded-full z-10"></button>
+      <button
+        className="group fixed flex flex-col items-center justify-center gap-1 text-sm shadow-lg bottom-5 right-5 md:bottom-10 md:right-10 bg-white h-10 w-10 lg:hover:h-16 rounded-full z-10 border border-slate-300 overflow-hidden duration-300"
+        onClick={() => {
+          window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+          });
+        }}
+      >
+        <FaArrowUpLong />
+        <span className="h-0 m-0 p-0 opacity-0 lg:group-hover:opacity-100 lg:group-hover:h-4 duration-300">
+          Top
+        </span>
+      </button>
     );
 }
 
